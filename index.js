7371b3588594011016d17b2abb8b23311f6cd8c8
Lily Eichler <lilyeichler61@gmail.com>
2020-10-27 00:48:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-function updateObjectWithKeyandValue(object, key, value) {
-  var newObj = Object.assign ({}, object)
-  newObj[key] = value
-  return newObj
-}
-
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
